d2e97a4ccb570d437d9d5ac39942db9ba44828ff
Rokkit-exe <francki0723@gmail.com>
2025-03-17 14:12:48 -0400

    ajout d'exercice complet

---
+++ exercices/exception/exec.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+class exec{
+	public static void main( String[] args ){
+		Scanner sc = new Scanner( System.in );
+		
+		String chaine;
+		char caractère = ' ';
+		int position = 0;
+		// Rattraper l'exception. À faire
+		+TODO                       -TODO
+			chaine = sc.next();
+			position = sc.nextInt();
+			caractère = chaine.+TODO           -TODO;
+			System.out.println( "Caractère " + caractère + " est à la position " + position );			
+			// +TODO
+// -TODO
+	}
+}
